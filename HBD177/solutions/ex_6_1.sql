eeb86c5640936587e320acc3c743b32a767899bb
i816013 <rich.heilman@sap.com>
2017-07-05 18:23:56 +0200

    a

--- HBD177/solutions/ex_6_1.sql
+++ HBD177/solutions/ex_6_1.sql
@@ -1,35 +1,16 @@
-DO ( OUT EX_TOP_3_EMP_PO_COMBINED_CNT TABLE(
-            FULLNAME NVARCHAR(256), 
-			CREATE_CNT INTEGER, 
-			CHANGE_CNT INTEGER,
-			COMBINED_CNT INTEGER ) => ?)
- BEGIN
- 
- PO_CREATE_CNT = SELECT COUNT(*) AS CREATE_CNT, "HISTORY.CREATEDBY.EMPLOYEEID" AS EID 
-         FROM "PO.Header" 
-            WHERE PURCHASEORDERID IN (
-        SELECT PURCHASEORDERID FROM "PO.Item" 
-             WHERE "PRODUCT.PRODUCTID" IS NOT NULL)
-          GROUP BY  "HISTORY.CREATEDBY.EMPLOYEEID";
-    
-PO_CHANGE_CNT = SELECT COUNT(*) AS CHANGE_CNT, "HISTORY.CHANGEDBY.EMPLOYEEID" AS EID
-           FROM "PO.Header"  
-             WHERE PURCHASEORDERID IN (
-             SELECT PURCHASEORDERID  FROM "PO.Item"
-          WHERE "PRODUCT.PRODUCTID" IS NOT NULL)
-           GROUP BY  "HISTORY.CHANGEDBY.EMPLOYEEID";
+PROCEDURE "get_product_by_filter" ( 
+      IN im_product_filter_string varchar(5000),
+      OUT ex_user_filtered_products "MD.Products" )
+   LANGUAGE SQLSCRIPT
+   SQL SECURITY INVOKER
+   --DEFAULT SCHEMA <default_schema_name>
+   READS SQL DATA AS
+BEGIN
 
-EX_TOP_3_EMP_PO_COMBINED_CNT = 
-   SELECT "get_full_name"("NAME.FIRST", "NAME.MIDDLE", "NAME.LAST") as FULLNAME,
-          crcnt.CREATE_CNT, chcnt.CHANGE_CNT,  
-          crcnt.CREATE_CNT + chcnt.CHANGE_CNT AS COMBINED_CNT
-       FROM "MD.Employees" as emp
-        LEFT OUTER JOIN :PO_CREATE_CNT AS crcnt
-            ON emp.EMPLOYEEID = crcnt.EID
-        LEFT OUTER JOIN :PO_CHANGE_CNT AS chcnt
-            ON emp.EMPLOYEEID = chcnt.EID
-           ORDER BY COMBINED_CNT DESC LIMIT 3;
- 
- 
- 
- END;
+ pre_filtered_products = SELECT * FROM "MD.Products"
+                              WHERE CATEGORY NOT IN ('Laser printers');
+       
+ ex_user_filtered_products = APPLY_FILTER(:pre_filtered_products, 
+                                         :im_product_filter_string ) ;
+
+END
